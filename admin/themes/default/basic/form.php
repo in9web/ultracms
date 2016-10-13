ed5138017d607983dc06ac7ba603bb729fe0d696
Wallace Silva <aliga12@yahoo.com.br>
2016-10-13 12:22:00 -0300

    fix theme admin form basic

--- admin/themes/default/basic/form.php
+++ admin/themes/default/basic/form.php
@@ -12,10 +12,15 @@ get_header_admin(); ?>
 $add_edit = 'add';
 if (strpos(Request::getCurrentUri(), $module.'/edit') !== false)
     $add_edit = 'edit';
-?>
-<?php if ($add_edit=='add'): ?>
 
-    <?php foreach ($columns_add as $col => $opt): ?>
+$columns_form = array();
+
+if ($add_edit=='add')
+    $columns_form = $columns_add;
+else
+    $columns_form = $columns_edit;
+?>
+    <?php foreach ($columns_form as $col => $opt): ?>
 
         <?php if ($opt['input_type']=='hidden'): ?>
             
@@ -45,34 +50,6 @@ if (strpos(Request::getCurrentUri(), $module.'/edit') !== false)
         <?php endif ?>
 
     <?php endforeach ?>
-    
-<?php else: ?>
-
-    <?php foreach ($columns_edit as $col => $opt): ?>
-
-        <?php if ($opt['input_type']=='hidden'): ?>
-            
-            <input type="<?php echo $opt['input_type'] ?>" class="form-control" id="input_<?php echo $col ?>" name="<?php echo $col ?>" value="<?php echo $item->$col ?>">
-
-        <?php elseif ($opt['input_type']=='textarea'): ?>
-
-            <div class="form-group">
-                <label for="input_<?php echo $col ?>"><?php t(ucfirst($col)) ?></label>
-                <textarea class="form-control" id="input_<?php echo $col ?>" name="<?php echo $col ?>" rows="3"><?php echo $item->$col ?></textarea>
-            </div>
-
-        <?php else: // default text ?>
-
-            <div class="form-group">
-                <label for="input_<?php echo $col ?>"><?php echo t(ucfirst($col)) ?></label>
-                <input type="<?php echo $opt['input_type'] ?>" class="form-control" id="input_<?php echo $col ?>" name="<?php echo $col ?>" placeholder="<?php echo ucfirst($col) ?>" value="<?php echo $item->$col ?>">
-            </div>
-
-        <?php endif ?>
-
-    <?php endforeach ?>
-
-<?php endif; ?>
 
     <div class="form-group">
         <button type="submit" class="btn btn-default"><?php t('Save') ?></button>
