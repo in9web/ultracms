8a16fdfbe765b2fd9a6a9957e913b1b2c0c284b2
Wallace Silva <aliga12@yahoo.com.br>
2017-09-22 03:04:33 -0300

    helpers to include ckeditor on admin themes
    this use cdn request to get ckeditor when invoked

--- admin/themes/default/functions.php
+++ admin/themes/default/functions.php
@@ -5,3 +5,11 @@ use Ultra\Assets;
 Assets::addAssetVendor('jquery-1.11.3');
 Assets::addAssetVendor('bootstrap-3.3.6');
 Assets::addAssetVendor('font-awesome-4.6.3');
+
+define('CKEDITOR_FILE_BROWSER', admin_url('/files/browser'));
+define('CKEDITOR_FILE_UPLOAD', admin_url('/files/upload'));
+
+function include_ckeditor($ckeditor_id)
+{
+    include(dirname(__FILE__).'/ckeditor.php');
+}
